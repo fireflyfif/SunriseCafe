188ec8fcad09118e15e9f5a5f89a44d01efe18a8
fireflyfif <fif.iva@gmail.com>
2017-03-08 21:09:19 +0100

    Scroll Down to the bottom of the layout

--- app/src/main/java/com/example/android/sunrisecafe/MainActivity.java
+++ app/src/main/java/com/example/android/sunrisecafe/MainActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,16 +16,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    // Jump to the bottom of the layout
+    public void scrollDown(View view) {
+
+        ScrollView scrollViewText = (ScrollView) findViewById(R.id.scroll_view);
+        scrollViewText.fullScroll(ScrollView.FOCUS_DOWN);
     }
 
 
-
-
+    // Increment Button
     public void increment(View view) {
         quantity = quantity + 1;
         display(quantity);
     }
 
+    // Decrement Button
     public void decrement(View view) {
         quantity = quantity - 1;
         display(quantity);
@@ -49,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
         quantityTextView.setText("" + number);
     }
     /**
-            * This method displays the given text on the screen.
+     * This method displays the given text on the screen.
     */
     private void displayMessage(String message) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
